2e80cb9b1f4a6c14bf51e757057d1a8895f1e706
Dhurgham Faris <d6r9g69@gmail.com>
2025-07-09 18:47:56 +0300

    on the way

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -1,1 +1,5 @@
 vim.keymap.set("n", "<leader>e", ":Oil --float<CR>", { desc = "Oil file explorer" })
+
+-- Fzf keymaps
+
+vim.keymap.set("n", "<leader>ff", ":FzfLua files<CR>", { desc = "Fzf find files" })
